a81928bcfda501880754e4ae0d6c5758ff570490
Dansuf <tomasz.ks9@gmail.com>
2018-01-12 20:10:57 +0100

    Optimized if conditions.

--- src/IntInstrIf.cpp
+++ src/IntInstrIf.cpp
@@ -10,27 +10,11 @@ IntInstrIf::IntInstrIf(std::string val1, std::string val2, Comparator comp, IntI
 
 IntInstrIf::IntInstrIf(std::string val1, std::string val2, Comparator comp, IntInstrBlock valInitBlock, IntInstrBlock block, IntInstrBlock elBlock) : val1(val1), val2(val2), comp(comp), block(block), elBlock(elBlock)
 {
-  switch(this->comp)
-  {
-    case Comparator::EQ:
-    case Comparator::NEQ:
-      this->subBlock = valInitBlock;
-      this->subBlock.addInstr((IntInstrAbstr*)new IntInstr(IntInstrType::SUB,"",this->val2,this->val1));
-      this->revSubBlock = valInitBlock;
-      this->revSubBlock.addInstr((IntInstrAbstr*)new IntInstr(IntInstrType::SUB,"",this->val1,this->val2));
-      break;
-    case Comparator::LT:
-    case Comparator::GTE:
-      this->subBlock = valInitBlock;
-      this->subBlock.addInstr((IntInstrAbstr*)new IntInstr(IntInstrType::SUB,"",this->val2,this->val1));
-      break;
-    case Comparator::GT:
-    case Comparator::LTE:
-      this->valInitBlock = valInitBlock;
-      this->subBlock = valInitBlock;
-      this->subBlock.addInstr((IntInstrAbstr*)new IntInstr(IntInstrType::SUB,"",this->val1,this->val2));
-      break;
-  }
+  this->valInitBlock = valInitBlock;
+  this->subBlock = valInitBlock;
+  this->subBlock.addInstr((IntInstrAbstr*)new IntInstr(IntInstrType::SUB,"",this->val2,this->val1));
+  this->revSubBlock = valInitBlock;
+  this->revSubBlock.addInstr((IntInstrAbstr*)new IntInstr(IntInstrType::SUB,"",this->val1,this->val2));
 }
 
 void IntInstrIf::dbgPrint()
@@ -82,25 +66,55 @@ InstructionRegistry IntInstrIf::translateEq(VariableRegistry& variables)
   if(!this->elBlock.empty()) cmds2 = variables.newLabel();
   lint endif = variables.newLabel();
 
-  lint endifAddr, cmds2Addr;
+  lint endifAddr, nextAddr, cmds1Addr, cmds2Addr;
 
-  InstructionRegistry ir = this->subBlock.translate(variables);
-  ir.addInstruction(Instr::JZERO,next);
-  lint nextAddr = ir.addInstruction(Instr::JUMP,this->elBlock.empty() ? endif : cmds2)+1;
-  ir.append(this->revSubBlock.translate(variables));
-  ir.addInstruction(Instr::JZERO,cmds1);
-  lint cmds1Addr = ir.addInstruction(Instr::JUMP,this->elBlock.empty() ? endif : cmds2) + 1;
-  if(this->elBlock.empty())
+  InstructionRegistry ir;
+
+  if(VariableRegistry::isConst(this->val2) && VariableRegistry::getConstVal(this->val2) <= 29 )
   {
+    int constant = VariableRegistry::getConstVal(this->val2);
+
+    ir = this->valInitBlock.translate(variables);
+    IntInstr::addLoadInstruction(variables,ir,this->val1);
+
+    for(int i = 0; i < constant; i++)
+    {
+      if(this->elBlock.empty())
+        ir.addInstruction(Instr::JZERO,endif);
+      else
+        ir.addInstruction(Instr::JZERO,cmds2);
+      ir.addInstruction(Instr::DEC);
+    }
+    cmds2Addr = ir.addInstruction(Instr::JZERO,cmds1) + 1;
+    if(this->elBlock.empty())
+      cmds1Addr = ir.addInstruction(Instr::JUMP,endif) + 1;
+    else
+    {
+      ir.append(this->elBlock.translate(variables));
+      cmds1Addr = ir.addInstruction(Instr::JUMP, endif) + 1;
+    }
+
     endifAddr = ir.append(this->block.translate(variables)) + 1;
   }
   else
   {
-    ir.append(this->block.translate(variables));
-    cmds2Addr = ir.addInstruction(Instr::JUMP,endif) + 1;
-    endifAddr = ir.append(this->elBlock.translate(variables)) + 1;
+    ir = this->subBlock.translate(variables);
+    ir.addInstruction(Instr::JZERO,next);
+    nextAddr = ir.addInstruction(Instr::JUMP,this->elBlock.empty() ? endif : cmds2)+1;
+    ir.append(this->revSubBlock.translate(variables));
+    ir.addInstruction(Instr::JZERO,cmds1);
+    cmds1Addr = ir.addInstruction(Instr::JUMP,this->elBlock.empty() ? endif : cmds2) + 1;
+    if(this->elBlock.empty())
+    {
+      endifAddr = ir.append(this->block.translate(variables)) + 1;
+    }
+    else
+    {
+      ir.append(this->block.translate(variables));
+      cmds2Addr = ir.addInstruction(Instr::JUMP,endif) + 1;
+      endifAddr = ir.append(this->elBlock.translate(variables)) + 1;
+    }
   }
-
 
   ir.setLabel(nextAddr,next);
   ir.setLabel(cmds1Addr,cmds1);
@@ -118,22 +132,51 @@ InstructionRegistry IntInstrIf::translateNeq(VariableRegistry& variables)
   if(!this->elBlock.empty()) cmds2 = variables.newLabel();
   lint endif = variables.newLabel();
 
-  lint endifAddr, cmds2Addr;
+  lint endifAddr, cmds1Addr, cmds2Addr, nextAddr;
 
-  InstructionRegistry ir = this->subBlock.translate(variables);
-  ir.addInstruction(Instr::JZERO,next);
-  lint nextAddr = ir.addInstruction(Instr::JUMP,cmds1) + 1;
-  ir.append(this->revSubBlock.translate(variables));
-  lint cmds1Addr = ir.addInstruction(Instr::JZERO,this->elBlock.empty() ? endif : cmds2) + 1;
-  if(this->elBlock.empty())
+  InstructionRegistry ir;
+
+  if(VariableRegistry::isConst(this->val2) && VariableRegistry::getConstVal(this->val2) <= 29 )
   {
+    int constant = VariableRegistry::getConstVal(this->val2);
+
+    ir = this->valInitBlock.translate(variables);
+    IntInstr::addLoadInstruction(variables,ir,this->val1);
+
+    for(int i = 0; i < constant; i++)
+    {
+      ir.addInstruction(Instr::JZERO,cmds1);
+      ir.addInstruction(Instr::DEC);
+    }
+    if(this->elBlock.empty())
+      cmds1Addr = ir.addInstruction(Instr::JZERO,endif) + 1;
+    else
+      cmds1Addr = ir.addInstruction(Instr::JZERO,cmds2) + 1;
+
     endifAddr = ir.append(this->block.translate(variables)) + 1;
+    if(!this->elBlock.empty())
+    {
+      cmds2Addr = ir.addInstruction(Instr::JUMP,endif) + 1;
+      endifAddr = ir.append(this->elBlock.translate(variables)) + 1;
+    }
   }
   else
   {
-    ir.append(this->block.translate(variables));
-    cmds2Addr = ir.addInstruction(Instr::JUMP,endif) + 1;
-    endifAddr = ir.append(this->elBlock.translate(variables)) + 1;
+    ir = this->subBlock.translate(variables);
+    ir.addInstruction(Instr::JZERO,next);
+    nextAddr = ir.addInstruction(Instr::JUMP,cmds1) + 1;
+    ir.append(this->revSubBlock.translate(variables));
+    cmds1Addr = ir.addInstruction(Instr::JZERO,this->elBlock.empty() ? endif : cmds2) + 1;
+    if(this->elBlock.empty())
+    {
+      endifAddr = ir.append(this->block.translate(variables)) + 1;
+    }
+    else
+    {
+      ir.append(this->block.translate(variables));
+      cmds2Addr = ir.addInstruction(Instr::JUMP,endif) + 1;
+      endifAddr = ir.append(this->elBlock.translate(variables)) + 1;
+    }
   }
 
   ir.setLabel(nextAddr,next);
@@ -147,26 +190,51 @@ InstructionRegistry IntInstrIf::translateNeq(VariableRegistry& variables)
 InstructionRegistry IntInstrIf::translateLt(VariableRegistry& variables)
 {
   lint endif = variables.newLabel();
+  lint cmds1 = variables.newLabel();
   lint cmds2;
   if(!this->elBlock.empty()) cmds2 = variables.newLabel();
 
-  InstructionRegistry ir = this->subBlock.translate(variables);
+  InstructionRegistry ir;
 
-  lint cmds2Addr, endifAddr;
+  lint cmds1Addr, cmds2Addr, endifAddr;
 
-  if(this->elBlock.empty())
+  if(VariableRegistry::isConst(this->val2) && VariableRegistry::getConstVal(this->val2) <= 2 )
   {
-    ir.addInstruction(Instr::JZERO,endif);
+    int val = VariableRegistry::getConstVal(this->val2);
+
+    ir = this->valInitBlock.translate(variables);
+    IntInstr::addLoadInstruction(variables,ir,this->val1);
+
+    ir.addInstruction(Instr::JZERO,cmds1);
+
+    for(int i = 1; i < val; i++)
+    {
+      ir.addInstruction(Instr::DEC);
+      ir.addInstruction(Instr::JZERO,cmds1);
+    }
+    ir.append(this->elBlock.translate(variables));
+    cmds1Addr = ir.addInstruction(Instr::JUMP,endif) + 1;
     endifAddr = ir.append(this->block.translate(variables)) + 1;
   }
   else
   {
-    ir.addInstruction(Instr::JZERO,cmds2);
-    ir.append(this->block.translate(variables));
-    cmds2Addr = ir.addInstruction(Instr::JUMP,endif) + 1;
-    endifAddr = ir.append(this->elBlock.translate(variables)) + 1;
+    ir = this->subBlock.translate(variables);
+
+    if(this->elBlock.empty())
+    {
+      ir.addInstruction(Instr::JZERO,endif);
+      endifAddr = ir.append(this->block.translate(variables)) + 1;
+    }
+    else
+    {
+      ir.addInstruction(Instr::JZERO,cmds2);
+      ir.append(this->block.translate(variables));
+      cmds2Addr = ir.addInstruction(Instr::JUMP,endif) + 1;
+      endifAddr = ir.append(this->elBlock.translate(variables)) + 1;
+    }
   }
 
+  ir.setLabel(cmds1Addr,cmds1);
   ir.setLabel(endifAddr,endif);
   if(!this->elBlock.empty()) ir.setLabel(cmds2Addr,cmds2);
 
@@ -181,18 +249,8 @@ InstructionRegistry IntInstrIf::translateGt(VariableRegistry& variables)
 
   lint endifAddr,cmds2Addr;
 
-  InstructionRegistry ir;
+  InstructionRegistry ir = this->revSubBlock.translate(variables);
 
-  if(VariableRegistry::isConst(this->val2) && VariableRegistry::getConstVal(this->val2) == 0) //TODO generalize
-  {
-    ir = this->valInitBlock.translate(variables);
-
-    IntInstr::addLoadInstruction(variables,ir,this->val1);
-  }
-  else
-  {
-    ir = this->subBlock.translate(variables);
-  }
 
   if(this->elBlock.empty())
   {
@@ -218,7 +276,7 @@ InstructionRegistry IntInstrIf::translateLte(VariableRegistry& variables)
   lint cmds1 = variables.newLabel();
   lint endif = variables.newLabel();
 
-  InstructionRegistry ir = this->subBlock.translate(variables);
+  InstructionRegistry ir = this->revSubBlock.translate(variables);
   ir.addInstruction(Instr::JZERO,cmds1);
   ir.append(this->elBlock.translate(variables));
   lint cmds1Addr = ir.addInstruction(Instr::JUMP,endif) + 1;
@@ -233,15 +291,50 @@ InstructionRegistry IntInstrIf::translateLte(VariableRegistry& variables)
 InstructionRegistry IntInstrIf::translateGte(VariableRegistry& variables)
 {
   lint cmds1 = variables.newLabel();
+  lint cmds2;
+  if(!this->elBlock.empty()) cmds2 = variables.newLabel();
   lint endif = variables.newLabel();
 
-  InstructionRegistry ir = this->subBlock.translate(variables);
-  ir.addInstruction(Instr::JZERO,cmds1);
-  ir.append(this->elBlock.translate(variables));
-  lint cmds1Addr = ir.addInstruction(Instr::JUMP,endif) + 1;
-  lint endifAddr = ir.append(this->block.translate(variables)) + 1;
+  lint cmds1Addr, cmds2Addr, endifAddr;
+
+  InstructionRegistry ir;
+
+  if(VariableRegistry::isConst(this->val2) && VariableRegistry::getConstVal(this->val2) <= 3 )
+  {
+    int val = VariableRegistry::getConstVal(this->val2);
+
+    ir = this->valInitBlock.translate(variables);
+    IntInstr::addLoadInstruction(variables,ir,this->val1);
+    if(this->elBlock.empty())
+      cmds1Addr = ir.addInstruction(Instr::JZERO,endif) + 1;
+    else
+      cmds1Addr = ir.addInstruction(Instr::JZERO,cmds2) + 1;
+    for(int i = 1; i < val; i++)
+    {
+      ir.addInstruction(Instr::DEC);
+      if(this->elBlock.empty())
+        cmds1Addr = ir.addInstruction(Instr::JZERO,endif) + 1;
+      else
+        cmds1Addr = ir.addInstruction(Instr::JZERO,cmds2) + 1;
+    }
+    endifAddr = ir.append(this->block.translate(variables)) + 1;
+    if(!this->elBlock.empty())
+    {
+      cmds2Addr = ir.addInstruction(Instr::JUMP,endif) + 1;
+      endifAddr = ir.append(this->elBlock.translate(variables)) + 1;
+    }
+  }
+  else
+  {
+    ir = this->subBlock.translate(variables);
+    ir.addInstruction(Instr::JZERO,cmds1);
+    ir.append(this->elBlock.translate(variables));
+    cmds1Addr = ir.addInstruction(Instr::JUMP,endif) + 1;
+    endifAddr = ir.append(this->block.translate(variables)) + 1;
+  }
 
   ir.setLabel(cmds1Addr,cmds1);
+  if(!this->elBlock.empty()) ir.setLabel(cmds2Addr,cmds2);
   ir.setLabel(endifAddr,endif);
 
   return ir;
@@ -252,21 +345,133 @@ InstructionRegistry IntInstrIf::translate(VariableRegistry& variables)
   switch(this->comp)
   {
     case Comparator::EQ:
+      if(VariableRegistry::isConst(this->val1) && !VariableRegistry::isConst(this->val2))
+      {
+        std::string tmp = this->val1;
+        this->val1 = this->val2;
+        this->val2 = tmp;
+      }
+      else if(VariableRegistry::isConst(this->val1) && VariableRegistry::isConst(this->val2))
+      {
+        if(VariableRegistry::getConstVal(this->val1) == VariableRegistry::getConstVal(this->val2))
+        {
+          return this->block.translate(variables);
+        }
+        else
+        {
+          return this->elBlock.translate(variables);
+        }
+      }
+      else
       return this->translateEq(variables);
       break;
     case Comparator::NEQ:
+      if(VariableRegistry::isConst(this->val1) && !VariableRegistry::isConst(this->val2))
+      {
+        std::string tmp = this->val1;
+        this->val1 = this->val2;
+        this->val2 = tmp;
+      }
+      else if(VariableRegistry::isConst(this->val1) && VariableRegistry::isConst(this->val2))
+      {
+        if(VariableRegistry::getConstVal(this->val1) != VariableRegistry::getConstVal(this->val2))
+        {
+          return this->block.translate(variables);
+        }
+        else
+        {
+          return this->elBlock.translate(variables);
+        }
+      }
+      else
       return this->translateNeq(variables);
       break;
     case Comparator::LT:
+      if(VariableRegistry::isConst(this->val1) && !VariableRegistry::isConst(this->val2))
+      {
+        std::string tmp = this->val1;
+        this->val1 = this->val2;
+        this->val2 = tmp;
+        return this->translateGt(variables);
+      }
+      else if(VariableRegistry::isConst(this->val1) && VariableRegistry::isConst(this->val2))
+      {
+        if(VariableRegistry::getConstVal(this->val1) < VariableRegistry::getConstVal(this->val2))
+        {
+          return this->block.translate(variables);
+        }
+        else
+        {
+          return this->elBlock.translate(variables);
+        }
+      }
+      else
       return this->translateLt(variables);
       break;
     case Comparator::GT:
+      if(VariableRegistry::isConst(this->val1) && !VariableRegistry::isConst(this->val2))
+      {
+        std::string tmp = this->val1;
+        this->val1 = this->val2;
+        this->val2 = tmp;
+        return this->translateLt(variables);
+      }
+      else if(VariableRegistry::isConst(this->val1) && VariableRegistry::isConst(this->val2))
+      {
+        if(VariableRegistry::getConstVal(this->val1) > VariableRegistry::getConstVal(this->val2))
+        {
+          return this->block.translate(variables);
+        }
+        else
+        {
+          return this->elBlock.translate(variables);
+        }
+      }
+      else
       return this->translateGt(variables);
       break;
     case Comparator::LTE:
+      if(VariableRegistry::isConst(this->val1) && !VariableRegistry::isConst(this->val2))
+      {
+        std::string tmp = this->val1;
+        this->val1 = this->val2;
+        this->val2 = tmp;
+        return this->translateGte(variables);
+      }
+      else if(VariableRegistry::isConst(this->val1) && VariableRegistry::isConst(this->val2))
+      {
+        if(VariableRegistry::getConstVal(this->val1) <= VariableRegistry::getConstVal(this->val2))
+        {
+          return this->block.translate(variables);
+        }
+        else
+        {
+          return this->elBlock.translate(variables);
+        }
+      }
+      else
       return this->translateLte(variables);
       break;
     case Comparator::GTE:
+      if(VariableRegistry::isConst(this->val1) && !VariableRegistry::isConst(this->val2))
+      {
+        std::string tmp = this->val1;
+        this->val1 = this->val2;
+        this->val2 = tmp;
+        return this->translateLte(variables);
+      }
+      else if(VariableRegistry::isConst(this->val1) && VariableRegistry::isConst(this->val2))
+      {
+        if(VariableRegistry::getConstVal(this->val1) >= VariableRegistry::getConstVal(this->val2))
+        {
+          return this->block.translate(variables);
+        }
+        else
+        {
+          return this->elBlock.translate(variables);
+        }
+      }
+      else
       return this->translateGte(variables);
       break;
   }
